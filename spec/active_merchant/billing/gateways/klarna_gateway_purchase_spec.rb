4d6d2f720c7a0a17b4a7c25f52002918d571cb73
Jose Antonio Pio Gil <josetonyp@latizana.com>
2016-12-12 10:58:54 +0100

    Fix bug in auto capture.

---
+++ spec/active_merchant/billing/gateways/klarna_gateway_purchase_spec.rb
@@ -0,0 +1,22 @@
+require "spec_helper"
+
+module ActiveMerchant
+  module Billing
+    describe KlarnaGateway do
+      extend KlarnaApiHelper
+
+      within_a_virtual_api do
+        # purchase(amount, payment_source, options={})
+        context "On purchase" do
+          it "calls to authorize and capture" do
+            expect(payment.payment_method.provider).to receive(:authorize).and_return(double('Response', success?: true))
+            expect(payment.payment_method.provider).to receive(:capture).and_return({})
+            payment.payment_method.provider.purchase(10, payment.source, options).tap do |response|
+              expect(response).to eq(response)
+            end
+          end
+        end
+      end
+    end
+  end
+end
