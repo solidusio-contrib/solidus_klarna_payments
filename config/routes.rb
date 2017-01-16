91c182b69fa30a05283f4f635a6a3ff05cac13f2
Jose Antonio Pio Gil <josetonyp@latizana.com>
2017-01-16 19:12:20 +0100

    Fix reauthorisation token change

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Spree::Core::Engine.routes.draw do
   post '/checkout/:state/klarna_session', to: 'checkout#klarna_session', as: :checkout_klarna_session
   get '/checkout/:state/klarna_order_status', to: 'checkout#order_status', as: :checkout_klarna_order_status
   get '/checkout/:state/klarna_order_addresses', to: 'checkout#order_addresses', as: :checkout_klarna_order_addresses
+  post '/checkout/:state/klarna_update_session', to: 'checkout#klarna_update_session', as: :checkout_klarna_update_session
 
   post '/klarna/notification', to: 'klarna#notification', as: :klarna_notification
 
