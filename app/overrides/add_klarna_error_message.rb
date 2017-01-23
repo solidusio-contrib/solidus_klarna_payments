3506ddab882f6e02cf54895aa9f98601c69d9778
Jose Antonio Pio Gil <josetonyp@latizana.com>
2017-01-23 16:48:33 +0100

    Remove Klarna warning message from Shipments
    As the information that is displayed in shipments will not affect
    Klarana's security information it is safe to modify it.

--- app/overrides/add_klarna_error_message.rb
+++ app/overrides/add_klarna_error_message.rb
@@ -1,11 +1,4 @@
 Deface::Override.new(
-  virtual_path: "spree/admin/orders/edit",
-  insert_top: "[data-hook='admin_order_edit_header']",
-  name: "add_klarna_error_message",
-  partial: "spree/admin/orders/error_message"
-)
-
-Deface::Override.new(
   virtual_path: "spree/admin/orders/customer_details/_form",
   insert_top: "[data-hook='admin_customer_detail_form_fields']",
   name: "add_klarna_error_message",
