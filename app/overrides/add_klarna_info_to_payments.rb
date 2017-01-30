f7fff1701f4be96b2a872f558a4618a2a04f41ef
Jose Antonio Pio Gil <josetonyp@latizana.com>
2017-01-30 13:16:05 +0100

    Include the Klarna information inside the Payment row
    Include missing deface override.

--- app/overrides/add_klarna_info_to_payments.rb
+++ app/overrides/add_klarna_info_to_payments.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
   virtual_path: "spree/admin/payments/_list",
-  insert_after: "[data-hook='payments_row']",
+  replace: "[data-hook='payments_row'] td:nth-child(4)",
   name: "add_klarna_info_to_payments",
   partial: "spree/admin/payments/klarna_info"
 )
