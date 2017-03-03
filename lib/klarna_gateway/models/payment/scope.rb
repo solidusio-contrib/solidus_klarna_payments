935f047d65f9dbd168b3ebd949887484f1bbb043
Pascal Jungblut <mail@pascal-jungblut.com>
2017-03-03 13:52:49 +0100

    Mark orders of REJECTED Klarna payments as risky
    Solidus checks for "risky" payments to identify an order as risky. It
    normally uses the ccv codes and "avs codes" from the address validation
    for that. This patch also includes Klarna's `REJECTED` payments as
    risky.
    
    See #8554

--- lib/klarna_gateway/models/payment/scope.rb
+++ lib/klarna_gateway/models/payment/scope.rb
@@ -4,6 +4,9 @@ module KlarnaGateway
       def self.included(base)
         base.class_eval do
           scope :klarna_credit, ->{ where(source_type: 'Spree::KlarnaCreditPayment') }
+          scope :risky, -> do
+            joins("JOIN spree_klarna_credit_payments ON spree_payments.source_id = spree_klarna_credit_payments.id")
+              .where("avs_response IN (?) OR (cvv_response_code IS NOT NULL and cvv_response_code != 'M') OR state = 'failed' OR (fraud_status = 'REJECTED' AND source_type = 'Spree::KlarnaCreditPayment')", Spree::Payment::RISKY_AVS_CODES)
         end
       end
     end
