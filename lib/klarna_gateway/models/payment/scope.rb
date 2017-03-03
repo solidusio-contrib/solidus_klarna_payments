7b7d894c7f8e4223bce4d81bf54df95c658fd264
Pascal Jungblut <mail@pascal-jungblut.com>
2017-03-03 14:55:39 +0100

    Add number of rejected Klarna payments to risk management

--- lib/klarna_gateway/models/payment/scope.rb
+++ lib/klarna_gateway/models/payment/scope.rb
@@ -7,6 +7,12 @@ module KlarnaGateway
           scope :risky, -> do
             joins("JOIN spree_klarna_credit_payments ON spree_payments.source_id = spree_klarna_credit_payments.id")
               .where("avs_response IN (?) OR (cvv_response_code IS NOT NULL and cvv_response_code != 'M') OR state = 'failed' OR (fraud_status = 'REJECTED' AND source_type = 'Spree::KlarnaCreditPayment')", Spree::Payment::RISKY_AVS_CODES)
+          end
+
+          scope :klarna_risky, -> do
+            joins("JOIN spree_klarna_credit_payments ON spree_payments.source_id = spree_klarna_credit_payments.id")
+              .where("fraud_status = 'REJECTED' AND source_type = 'Spree::KlarnaCreditPayment'")
+          end
         end
       end
     end
