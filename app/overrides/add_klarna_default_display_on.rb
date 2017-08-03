e656703f8116ec2d41238fc611cc9696624678fa
Jose Antonio Pio Gil <josetonyp@latizana.com>
2017-08-03 13:46:11 +0200

    Update selection for newer versions of Solidus.

--- app/overrides/add_klarna_default_display_on.rb
+++ app/overrides/add_klarna_default_display_on.rb
@@ -1,8 +1,6 @@
-if Gem::Version.new(Spree.solidus_version) < Gem::Version.new('2.0.0')
-  Deface::Override.new(
-    virtual_path: "spree/admin/payment_methods/_form",
-    insert_after: "[data-hook='admin_payment_method_form_fields']",
-    name: "add_klarna_default_display_on",
-    partial: "spree/admin/payment_methods/default_display_on.erb"
-  )
-end
+Deface::Override.new(
+  virtual_path: "spree/admin/payment_methods/_form",
+  insert_after: "[data-hook='admin_payment_method_form_fields']",
+  name: "add_klarna_default_display_on",
+  partial: "spree/admin/payment_methods/default_display_on"
+)
