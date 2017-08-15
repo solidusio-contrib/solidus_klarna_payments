2ef1fb79bd49f388428054e69d8a31244f095968
Tinus Wagner <tinus@3months.com>
2017-08-15 13:51:23 +0200

    wrapping data constructors in methods so that each test has access to a fresh construct with a new object id

--- spec/support/fixtures/data.rb
+++ spec/support/fixtures/data.rb
@@ -16,84 +16,95 @@ class TestData
       pending_rejected: 'user+pend-reject-05@example.com',
     })
 
-  UKAddress = Address.new(
-                'Testing User',
-                'UK person',
-                '222 Oxford st',
-                'London',
-                'United Kingdom',
-                'London, City of',
-                'W1C 1DD',
-                '2076367700',
-                nil,
-                'youremail@email.com'   # Email
-                )
+  def uk_address
+    Address.new(
+      'Testing User',
+      'UK person',
+      '222 Oxford st',
+      'London',
+      'United Kingdom',
+      'London, City of',
+      'W1C 1DD',
+      '2076367700',
+      nil,
+      'youremail@email.com'   # Email
+    )
+  end
 
-  USAddress = Address.new(
-                'Testing User',             # Name
-                'US person',                # Last Name
-                '1436 Adriel Dr',           # Stree
-                'Fort Collins',             # City
-                'United States of America', # Country
-                'Colorado',                 # Distric
-                '80524',                    # Postal Code
-                '4075631020',               # Phone
-                nil,                        # Age
-                'youremail@email.com'       # Email
-                )
+  def us_address
+    return Address.new(
+      'Testing User',             # Name
+      'US person',                # Last Name
+      '1436 Adriel Dr',           # Stree
+      'Fort Collins',             # City
+      'United States of America', # Country
+      'Colorado',                 # Distric
+      '80524',                    # Postal Code
+      '4075631020',               # Phone
+      nil,                        # Age
+      'youremail@email.com'       # Email
+    )
+  end
 
-  DEAddress = Address.new(
-                'Testperson-de',        # Name
-                'Approved',             # Last Name
-                'Hellersbergstraße 14', # Stree
-                'Neuss',                # City
-                'Germany',              # Country
-                'Nordrhein-Westfalen',  # Distric
-                '41460',                # Postal Code
-                '01522113356',          # Phone
-                '07.07.1960',           # Age
-                'user@example.com'   # Email
-                )
+  def de_address
+    Address.new(
+      'Testperson-de',        # Name
+      'Approved',             # Last Name
+      'Hellersbergstraße 14', # Stree
+      'Neuss',                # City
+      'Germany',              # Country
+      'Nordrhein-Westfalen',  # Distric
+      '41460',                # Postal Code
+      '01522113356',          # Phone
+      '07.07.1960',           # Age
+      'user@example.com'   # Email
+    )
+  end
 
-  SEAddress = Address.new(
-                'Testperson-se',        # Name
-                'Approved',             # Last Name
-                'Stårgatan 1',          # Stree
-                'Ankeborg',             # City
-                'Sweden',              # Country
-                'Jönköpings län',       # Distric
-                '12345',                # Postal Code
-                '0765260000',           # Phone
-                nil,                    # Age
-                'youremail@email.com'   # Email
-                )
+  def se_address
+    Address.new(
+      'Testperson-se',        # Name
+      'Approved',             # Last Name
+      'Stårgatan 1',          # Stree
+      'Ankeborg',             # City
+      'Sweden',              # Country
+      'Jönköpings län',       # Distric
+      '12345',                # Postal Code
+      '0765260000',           # Phone
+      nil,                    # Age
+      'youremail@email.com'   # Email
+    )
+  end
 
-  NOAddress = Address.new(
-                'Testperson-no',        # Name
-                'Approved',             # Last Name
-                'Sæffleberggate 56',    # Stree
-                'Oslo',                 # City
-                'Norway',              # Country
-                'Oslo',                 # Distric
-                '0563',                 # Postal Code
-                '40 123 456',           # Phone
-                nil,                    # Age
-                'user@example.com'      # Email
-                )
+  def no_address
+    Address.new(
+      'Testperson-no',        # Name
+      'Approved',             # Last Name
+      'Sæffleberggate 56',    # Stree
+      'Oslo',                 # City
+      'Norway',              # Country
+      'Oslo',                 # Distric
+      '0563',                 # Postal Code
+      '40 123 456',           # Phone
+      nil,                    # Age
+      'user@example.com'      # Email
+    )
+  end
 
-  FIAddress = Address.new(
-                'Testperson-fi',        # Name
-                'Approved',             # Last Name
-                'Kiväärikatu 10',       # Stree
-                'Pori',                 # City
-                'Finland',              # Country
-                'Satakunta',            # Distric
-                '28100',                # Postal Code
-                '0401234567',           # Phone
-                nil,                    # Age
-                'user@example.com'      # Email
-                )
-
+  def fi_address
+    Address.new(
+      'Testperson-fi',        # Name
+      'Approved',             # Last Name
+      'Kiväärikatu 10',       # Stree
+      'Pori',                 # City
+      'Finland',              # Country
+      'Satakunta',            # Distric
+      '28100',                # Postal Code
+      '0401234567',           # Phone
+      nil,                    # Age
+      'user@example.com'      # Email
+    )
+  end
 
   def initialize(store_id)
     @store_id = store_id
@@ -104,19 +115,19 @@ class TestData
   end
 
   def address
-    case @store_id
+    @address ||= case @store_id
       when 'uk'
-        UKAddress
+        uk_address
       when 'de'
-        DEAddress
+        de_address
       when 'se'
-        SEAddress
+        se_address
       when 'no'
-        NOAddress
+        no_address
       when 'fi'
-        FIAddress
+        fi_address
       else
-        USAddress
+        us_address
     end
   end
 
