c84d2214ed08288e750543f2105d765640bfb8b9
Tinus Wagner <tinus@3months.com>
2017-08-25 11:19:07 +0200

    spec for legacy checkout written and passing

---
+++ spec/features/orders/placing_non_klarna_order_spec.rb
@@ -0,0 +1,17 @@
+require 'features_helper'
+
+describe 'Orders with non-klarna payment method renders legacy checkout', type: 'feature', bdd: true, no_klarna: true do
+  include_context "ordering with klarna"
+  include WorkflowDriver::Process
+
+  it 'Should not render the klarna iframe if check is payment method' do
+    order_product(product_name:  'Ruby on Rails Bag', testing_data: @testing_data)
+
+    on_the_payment_page do |page|
+      expect(page.displayed?).to be(true)
+      page.select_payment_method("Check").click
+
+      expect(page).not_to have_selector('iframe')
+    end
+  end
+end
