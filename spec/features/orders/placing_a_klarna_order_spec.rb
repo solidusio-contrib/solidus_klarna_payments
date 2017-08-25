4134656847ab9d798c8dbe101e6a9791b984f64e
Tinus Wagner <tinus@3months.com>
2017-08-25 12:48:48 +0200

    feature test for Klarna signup flow written, pending Klarna adding the feature from their end

--- spec/features/orders/placing_a_klarna_order_spec.rb
+++ spec/features/orders/placing_a_klarna_order_spec.rb
@@ -22,6 +22,19 @@ describe 'Ordering with Klarna Payment Method', type: 'feature', bdd: true do
     Capybara.current_session.driver.quit
   end
 
+  # This is not implimented in Klarna yet
+  xit 'completes a klarna purchase once user has completed signup via klarna iframe' do
+    order_product(product_name: 'Ruby on Rails Bag', email: TestData::Users.signup, testing_data: @testing_data)
+    on_the_payment_page do |page|
+      expect(page.displayed?).to be(true)
+
+      page.select_klarna(@testing_data)
+      page.klarna_credit do |frame|
+        expect(frame).to have_content('Register')
+      end
+    end
+  end
+
   # Due to a bug in Solidus this test is currently not working
   # see https://github.com/solidusio/solidus/pull/542
   xit 'can change to a check payment before confirming the payment' do
