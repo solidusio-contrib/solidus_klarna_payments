9f9519f6e6952877de0dbc8fadc8cec1b940205c
Tinus Wagner <tinus@3months.com>
2017-08-25 13:15:56 +0200

    refining spec for non klarna method

--- spec/features/orders/placing_non_klarna_order_spec.rb
+++ spec/features/orders/placing_non_klarna_order_spec.rb
@@ -12,12 +12,17 @@ describe 'Orders with non-klarna payment method renders legacy checkout', type: 
       page.select_payment_method("Check").click
 
       expect(page).not_to have_selector('iframe')
-      page.continue
     end
+  end
 
-    on_the_complete_page do |page|
+  it 'Should not render the klarna iframe if Credit Card is payment method' do
+    order_product(product_name:  'Ruby on Rails Bag', testing_data: @testing_data)
+
+    on_the_payment_page do |page|
       expect(page.displayed?).to be(true)
-      expect(page.notice).to be_displayed
+      page.select_payment_method("Credit Card").click
+
+      expect(page).not_to have_selector('iframe')
     end
   end
 end
