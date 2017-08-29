f33322fceadb8aa1be99d18a3b15b698d49c556e
Tinus Wagner <tinus@3months.com>
2017-08-29 17:22:49 +0200

    adding meaningful comment to us specific test

--- spec/features/orders/klarna_us_region_form_disabled_spec.rb
+++ spec/features/orders/klarna_us_region_form_disabled_spec.rb
@@ -3,7 +3,8 @@ require 'features_helper'
 describe 'Disables Klarna payment options if the form is toggled false (US Specific)', type: 'feature', bdd: true  do
   include_context "ordering with klarna"
   include WorkflowDriver::Process
-
+  
+  # This is only implemented for US
   it 'Klarna form renders error instead of options (US Specific)', only: :us do
     @testing_data.address.email = TestData::Users.no_options_available
     order_product(product_name: 'Ruby on Rails Bag', testing_data: @testing_data)
