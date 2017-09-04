136543e7f08708f0bcea5b7d0290cdf4d1f9ccec
Tinus Wagner <tinus@3months.com>
2017-09-04 17:24:58 +0200

    using Gateway helper instead of environment variable

--- spec/support/page_drivers/admin/payment.rb
+++ spec/support/page_drivers/admin/payment.rb
@@ -1,10 +1,10 @@
 module PageDrivers
   module Admin
     class PaymentMenu < SitePrism::Section
-      if ENV["SOLIDUS_VERSION"] == "~> 1.4.0"
-        element :logs, :xpath,'//*[@id="content-header"]/ul/li/a'
-      else
+      if KlarnaGateway.up_to_solidus?("1.3.99")
         element :logs, 'a[icon="archive"]'
+      else
+        element :logs, :xpath,'//*[@id="content-header"]/ul/li/a'
       end
     end
 
