0148ad8d98dcb902c5e9494a42db996d13ff6794
Alessandro Desantis <desa.alessandro@gmail.com>
2020-06-11 14:09:46 +0200

    Fix coding style issues in KlarnaHelper

--- app/helpers/spree/klarna_helper.rb
+++ app/helpers/spree/klarna_helper.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 module Spree
   module KlarnaHelper
-    def cents_to_display_money(cents, currency=nil)
-      Spree::Money.new(BigDecimal.new(cents) / 100, currency: currency).to_html
+    def cents_to_display_money(cents, currency = nil)
+      Spree::Money.new(BigDecimal(cents) / 100, currency: currency).to_html
     end
   end
 end
