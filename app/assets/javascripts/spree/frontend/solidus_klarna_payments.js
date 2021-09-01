f3622845d24b867935464e06ad52200547300572
vassalloandrea <andrea.vassallo.94@gmail.com>
2021-09-01 11:35:29 +0200

    Format the main script using Prettier

--- app/assets/javascripts/spree/frontend/solidus_klarna_payments.js
+++ app/assets/javascripts/spree/frontend/solidus_klarna_payments.js
@@ -1,52 +1,66 @@
 "use strict";
-(function(KlarnaGateway, $) {
-  $.fn.klarnaAuthorize = function(options) {
-    var settings = $.extend({
-      authorizationToken: $("#klarna_authorization_token", this),
-      form: $("#checkout_form_payment"),
-      klarnaSelected: function (settings) {
-        return settings.paymentChangedElements.filter(":checked").val() === settings.paymentId.toString();
+(function (KlarnaGateway, $) {
+  $.fn.klarnaAuthorize = function (options) {
+    var settings = $.extend(
+      {
+        authorizationToken: $("#klarna_authorization_token", this),
+        form: $("#checkout_form_payment"),
+        klarnaSelected: function (settings) {
+          return (
+            settings.paymentChangedElements.filter(":checked").val() ===
+            settings.paymentId.toString()
+          );
+        },
+        loadDirectly: false,
+        onSubmit: function () {},
+        onAbort: function () {},
+        paymentChangedElements: $(
+          'input[name="order[payments_attributes][][payment_method_id]"]'
+        ),
+        paymentId: $(this).data("payment-method-id"),
+        paymentMethodWrapper: $(".form-payment-method-klarna_credit"),
+        preferredPaymentMethod: $(this).data("preferred-payment-method"),
+        sessionUrl: Spree.urlForDomain(Spree.pathFor("klarna/session")),
+        submitButton: $("form.edit_order :submit"),
       },
-      loadDirectly: false,
-      onSubmit: function() {},
-      onAbort: function() {},
-      paymentChangedElements: $("input[name=\"order[payments_attributes][][payment_method_id]\"]"),
-      paymentId: $(this).data("payment-method-id"),
-      paymentMethodWrapper: $(".form-payment-method-klarna_credit"),
-      preferredPaymentMethod: $(this).data("preferred-payment-method"),
-      sessionUrl: Spree.urlForDomain(Spree.pathFor("klarna/session")),
-      submitButton: $("form.edit_order :submit"),
-    }, options);
+      options
+    );
 
     // Get a session from the backend and load the form
     function initSession() {
-
       // Try to create a new session in the backend
       Spree.ajax({
         method: "POST",
         url: settings.sessionUrl,
-        data: {klarna_payment_method_id: settings.paymentId}
-      }).success(function(response) {
-        if (!response.token) {
-          window.console && console.log("[Klarna Credit] received empty token:", response);
+        data: { klarna_payment_method_id: settings.paymentId },
+      })
+        .success(function (response) {
+          if (!response.token) {
+            window.console &&
+              console.log("[Klarna Credit] received empty token:", response);
+            displayError();
+            return;
+          }
+          settings.clientToken = response.token;
+
+          // Initialize the Klarna Credit session in the frontend
+          Klarna.Credit.init({
+            client_token: response.token,
+          });
+
+          // Only load the iframe when Klarna is selected
+          if (settings.loadDirectly || settings.klarnaSelected(settings)) {
+            loadKlarnaForm();
+          }
+        })
+        .error(function (response) {
+          window.console &&
+            console.log(
+              "[Klarna Credit] received erroneous server response:",
+              response
+            );
           displayError();
-          return;
-        }
-        settings.clientToken = response.token;
-
-        // Initialize the Klarna Credit session in the frontend
-        Klarna.Credit.init ({
-          client_token: response.token
         });
-
-        // Only load the iframe when Klarna is selected
-        if (settings.loadDirectly || settings.klarnaSelected(settings)) {
-          loadKlarnaForm();
-        }
-      }).error(function(response) {
-        window.console && console.log("[Klarna Credit] received erroneous server response:", response);
-        displayError();
-      });
     }
 
     function displayError() {
@@ -56,7 +70,9 @@
 
     function denied() {
       $(".klarna_error.denied_error").show();
-      settings.paymentChangedElements.filter("[value=\"" + settings.paymentId + "\"]").attr("disabled", true);
+      settings.paymentChangedElements
+        .filter('[value="' + settings.paymentId + '"]')
+        .attr("disabled", true);
     }
 
     // Loads the Klarna Form
@@ -65,17 +81,20 @@
         return;
       }
 
-      Klarna.Credit.load ({
-        container: "#klarna_container",
-        preferred_payment_method: settings.preferredPaymentMethod
-      }, function(res) {
-        if (res.show_form) {
-          settings.showForm = res.show_form;
-        } else {
-          settings.paymentMethodWrapper.hide();
-          window.console && console.log(res);
+      Klarna.Credit.load(
+        {
+          container: "#klarna_container",
+          preferred_payment_method: settings.preferredPaymentMethod,
+        },
+        function (res) {
+          if (res.show_form) {
+            settings.showForm = res.show_form;
+          } else {
+            settings.paymentMethodWrapper.hide();
+            window.console && console.log(res);
+          }
         }
-      });
+      );
     }
 
     // Try to authorize
@@ -83,11 +102,13 @@
       // First get the current, serialized order
       Spree.ajax({
         method: "GET",
-        url: Spree.urlForDomain(Spree.pathFor("/klarna/session/order_addresses")),
+        url: Spree.urlForDomain(
+          Spree.pathFor("/klarna/session/order_addresses")
+        ),
         dataType: "json",
-        data: {klarna_payment_method_id: settings.paymentId}
-      }).done(function(result) {
-        Klarna.Credit.authorize(result, function(res) {
+        data: { klarna_payment_method_id: settings.paymentId },
+      }).done(function (result) {
+        Klarna.Credit.authorize(result, function (res) {
           if (res.approved === true) {
             settings.authorizationToken.val(res.authorization_token);
             approved(res);
@@ -98,14 +119,16 @@
       });
     }
 
-
     // Revert Spree"s disableSaveOnClick when authorization is denied
     function enableSaveOnClick() {
-      settings.submitButton.attr("disabled", false).addClass("primary").removeClass("disabled");
+      settings.submitButton
+        .attr("disabled", false)
+        .addClass("primary")
+        .removeClass("disabled");
     }
 
     // Check whether Klarna is selected and load the form
-    settings.paymentChangedElements.on("change", function() {
+    settings.paymentChangedElements.on("change", function () {
       // check if Klarna Credit is selected
       if (settings.klarnaSelected(settings)) {
         loadKlarnaForm();
@@ -118,20 +141,23 @@
       if (settings.klarnaSelected(settings)) {
         e.preventDefault();
         settings.onSubmit(settings);
-        authorize(function (result) {
-          if (result.approved) {
-            form.submit();
-          } else {
+        authorize(
+          function (result) {
+            if (result.approved) {
+              form.submit();
+            } else {
+              settings.onAbort(settings);
+              enableSaveOnClick();
+            }
+          },
+          function (result) {
+            if (result.approved == false) {
+              denied();
+            }
             settings.onAbort(settings);
             enableSaveOnClick();
           }
-        }, function(result) {
-          if (result.approved == false) {
-            denied();
-          }
-          settings.onAbort(settings);
-          enableSaveOnClick();
-        });
+        );
       }
     });
 
@@ -139,16 +165,16 @@
     return this;
   };
 
-  KlarnaGateway.loadSdk = function(w, d, callback) {
+  KlarnaGateway.loadSdk = function (w, d, callback) {
     var url = "https://credit.klarnacdn.net/lib/v1/api.js";
     var n = d.createElement("script");
     var c = d.getElementById("klarna-credit-lib-x");
     n.async = !0;
-    n.src = url + "?" + (new Date ()).getTime();
+    n.src = url + "?" + new Date().getTime();
     c.parentNode.replaceChild(n, c);
     n.onload = callback;
   };
-}(window.KlarnaGateway = window.KlarnaGateway || {}, jQuery));
+})((window.KlarnaGateway = window.KlarnaGateway || {}), jQuery);
 
 Spree.urlForDomain = function (uri, query) {
   if (uri.path === undefined) {
