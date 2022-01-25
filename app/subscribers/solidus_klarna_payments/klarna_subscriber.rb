b551aa0a4baf71b14822280950da082127b46a69
Daniele Palombo <387690+DanielePalombo@users.noreply.github.com>
2022-01-25 16:16:33 +0100

    Fix subscription errors
    - type on event_name order_recalulated (missing `c`)
    - the subscriber receives an argument

--- app/subscribers/solidus_klarna_payments/klarna_subscriber.rb
+++ app/subscribers/solidus_klarna_payments/klarna_subscriber.rb
@@ -4,15 +4,15 @@ module SolidusKlarnaPayments
   module KlarnaSubscriber
     include ::Spree::Event::Subscriber
 
-    event_action :update_klarna_shipments, event_name: :order_recalulated
-    event_action :update_klarna_customer, event_name: :order_recalulated
+    event_action :update_klarna_shipments, event_name: :order_recalculated
+    event_action :update_klarna_customer, event_name: :order_recalculated
 
-    def update_klarna_shipments
+    def update_klarna_shipments(event)
       order = event.payload[:order]
       order.update_klarna_shipments
     end
 
-    def update_klarna_customer
+    def update_klarna_customer(event)
       order = event.payload[:order]
       order.update_klarna_customer
     end
