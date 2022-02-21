80044ccc37e60cde47c2461c21776f8399ffb352
Daniele Palombo <387690+DanielePalombo@users.noreply.github.com>
2022-02-21 20:49:17 +0100

    Add API Session controller
    SolidusKlarnaPayments::SessionController inherits from
    Spree::StoreController which is defined under solidus_frontend.
    In order to avoid to include solidus_frontend, this commit adds another
    session controller under api.

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,8 @@ SolidusKlarnaPayments::Engine.routes.draw do
   post '/sessions', to: '/solidus_klarna_payments/sessions#create'
   get '/sessions', to: '/solidus_klarna_payments/sessions#show'
   get '/sessions/order_addresses', to: '/solidus_klarna_payments/sessions#order_addresses'
+
+  post '/api/sessions', to: '/solidus_klarna_payments/api/sessions#create'
+  get '/api/sessions', to: '/solidus_klarna_payments/api/sessions#show'
+  get '/api/sessions/order_addresses', to: '/solidus_klarna_payments/api/sessions#order_addresses'
 end
