9863ce3fa49986bd00bc35cfeef05b5ada010e2e
Daniele Palombo <387690+DanielePalombo@users.noreply.github.com>
2022-03-25 17:44:32 +0100

    Fix callback controller
    Api controllers shouldn't inherit from Spree::StoreController because
    some stores doesn't have the frontend.

--- app/controllers/solidus_klarna_payments/api/base_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module SolidusKlarnaPayments
-  module Api
-    class BaseController < ::Spree::StoreController
-      skip_before_action :verify_authenticity_token
-    end
-  end
-end
